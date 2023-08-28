7639650b1fc384cc3af47f7362fe3802d1c1b975
Mark Slorach <hello@markslorach.com>
2023-08-28 10:18:13 +0100

    question and answer adding to database through test

--- Java/capstone/src/test/java/com/capstone/example/capstone/CapstoneApplicationTests.java
+++ Java/capstone/src/test/java/com/capstone/example/capstone/CapstoneApplicationTests.java
@@ -1,6 +1,11 @@
 package com.capstone.example.capstone;
 
+import com.capstone.example.capstone.models.Answer;
+import com.capstone.example.capstone.models.Question;
+import com.capstone.example.capstone.models.QuestionLevel;
 import com.capstone.example.capstone.models.User;
+import com.capstone.example.capstone.repositories.AnswerRepository;
+import com.capstone.example.capstone.repositories.QuestionRepository;
 import com.capstone.example.capstone.repositories.UserRepository;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
@@ -12,14 +17,32 @@ class CapstoneApplicationTests {
 
 	@Autowired
 	UserRepository userRepository;
+	@Autowired
+	QuestionRepository questionRepository;
+	@Autowired
+	AnswerRepository answerRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	public void AddUser(){
+	public void addUser(){
 		User jimmy = new User("Jimmy", 3, 15, 2, "WeeJimmy", "password");
 		userRepository.save(jimmy);
+	}
+
+//	@Test
+//	public void addQuestion(){
+//		Question question = new Question("This is a question", "This is a hint", 2, QuestionLevel.BEGINNER);
+//		questionRepository.save(question);
+//	}
+
+	@Test
+	public void addQuestionAndAnswer(){
+		Question question = new Question("This is a question", "This is a hint", 2, QuestionLevel.BEGINNER);
+		questionRepository.save(question);
+		Answer answer = new Answer("This is an answer", true, question);
+		answerRepository.save(answer);
 	}
 }
