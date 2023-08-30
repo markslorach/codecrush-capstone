de4130c6dcc4d987e112cd9ff1975831a594caa2
Mark Slorach <hello@markslorach.com>
2023-08-30 15:55:33 +0100

    working database with users

--- codecrush/app/context/AuthContext.js
+++ codecrush/app/context/AuthContext.js
@@ -1,23 +1,60 @@
 import { useContext, createContext, useState, useEffect } from "react";
 import {signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider} from "firebase/auth";
 import { auth } from "../Firebase";
+import Request from "../helpers/Request";
+
 
 const AuthContext = createContext();
 export const AuthContextProvider = ({children}) => {
 const [user, setUser] = useState(null)
+console.log(user);
+
+
+
 const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
+    
 }
 const logOut = () => {
     signOut(auth);
 }
 useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth,(currentUser) => {
+        if (currentUser !== null) {
+        console.log(currentUser)
+        const createdUser = createNewUser(currentUser)
+      addUser(createdUser)
     setUser(currentUser)
+        }
+
     })
     return () => unsubscribe();
 }, [user]);
+
+const createNewUser = (user) => {
+    
+
+    const newUser = {
+        "streak":0, 
+        "score": 0,
+        "username": user.displayName,
+        "email": user.email 
+    
+    };
+    
+
+return newUser
+}
+
+
+const addUser = (newUser) => {
+const request = new Request();
+request.post('http://localhost:8082/api/users', newUser) 
+
+
+
+}
 return (
 <AuthContext.Provider value={{user, googleSignIn, logOut}}>{children}</AuthContext.Provider>)
 }
--- codecrush/app/login/LoginComponent.jsx
+++ codecrush/app/login/LoginComponent.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { UserAuth } from "../context/AuthContext";
+import Request from "../helpers/Request";
 
 export default function LoginComponent() {
-  const { user, googleSignIn, logOut } = UserAuth();
+  const { user, googleSignIn, logOut} = UserAuth();
   const handleSignIn = async () => {
     try {
       await googleSignIn();
+      
     } catch (error) {
       console.log(error);
     }
   };
   console.log(user);
+
+ 
   return (
     <main>
       <button onClick={handleSignIn}>Login</button>
