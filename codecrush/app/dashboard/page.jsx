b2304a7900d1d55a7d4d270c82e2ae07f0f10454
Mark Slorach <hello@markslorach.com>
2023-09-02 14:10:32 +0100

    updated

--- codecrush/app/dashboard/page.jsx
+++ codecrush/app/dashboard/page.jsx
@@ -5,23 +5,20 @@ import { PythonDifficultySelect } from "./PythonDifficultySelect";
 import { UserScore } from "../profile/UserScore";
 import { UserStreak } from "../profile/UserStreak";
 
-
 export default function Dashboard() {
   const [isPythonModalOpen, setIsPythonModalOpen] = useState(false);
 
   return (
-    
-    <main>
-
+    <main className="p-6">
       <h2>Dashboard</h2>
 
-      <h3 className="flex space-x-2">
+      <h2 className="flex space-x-2">
         <span>Hello,</span>
         <UserName />
         <span>👋</span>
-      </h3>
+      </h2>
 
-      <h3>Stats.</h3>
+      <h2>Stats.</h2>
 
       <p>Score:</p>
       <UserScore />
@@ -29,20 +26,24 @@ export default function Dashboard() {
       <p>Streak:</p>
       <UserStreak />
 
-      <h3>Today's challenge.</h3>
+      <h2>Today's challenge.</h2>
 
       <button
-        className="p-2 bg-red-300"
+        className="flex flex-col gap-2 items-center p-2 bg-red-700"
         onClick={() => setIsPythonModalOpen(true)}>
-        Python
+      
+        <img src="image_url" alt="image" className="w-full h-1/3" />
+        <h3 className="text-center my-0">Python</h3>
+        <p className="my-0 py-1">Description</p>
+
       </button>
 
       <PythonDifficultySelect
         isOpen={isPythonModalOpen}
-        setIsOpen={setIsPythonModalOpen}/>
-      
-      <button className="p-2 bg-blue-300">JavaScript</button>
+        setIsOpen={setIsPythonModalOpen}
+      />
 
+      <button className="p-2 bg-blue-300">JavaScript</button>
     </main>
   );
 }
