a35a495f5babfd9d89b70d4ce5bae25fa8ec2dff
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2023-09-05 15:10:52 +0100

    ;;

--- codecrush/app/leaderboard/LeaderboardComponent.jsx
+++ codecrush/app/leaderboard/LeaderboardComponent.jsx
@@ -24,7 +24,8 @@ export const LeaderboardComponent = () => {
     return (
       <div key={index}>
         <p className="py-1">
-          {firstName} - {user.score}
+          {firstName} - {user.score} 
+          {user.scoreLeaderboard}
         </p>
       </div>
     );
